2a9b30f8a960c71614b561ad7c410338438fd1f5
“Amin <aminkhan251199@gmail.com>
2023-04-24 20:51:28 +0530

    first commit

---
+++ Recursion.java
@@ -0,0 +1,18 @@
+package com.datastructer.practice;
+
+public class Recursion {
+
+    private static void fun() {
+        System.out.println(1);
+        fun();
+    }
+
+    public static void main(String[] args) {
+
+        fun();
+    }
+
+
+}
+
+
